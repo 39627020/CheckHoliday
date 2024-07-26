5c6db5b7661c05080c8d806d6aed66c3feb72dbf
jiangzd <jzd1997@hotmail.com>
2024-07-26 23:51:23 +0900

    整合swagger成功

--- src/main/java/com/jzd1997/checkholiday/controller/HolidayController.java
+++ src/main/java/com/jzd1997/checkholiday/controller/HolidayController.java
@@ -13,6 +13,8 @@ import com.jzd1997.checkholiday.domain.Holiday;
 import com.jzd1997.checkholiday.domain.Result;
 import com.jzd1997.checkholiday.service.IHolidayService;
 
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/holidays")
 public class HolidayController {
@@ -20,7 +22,7 @@ public class HolidayController {
     IHolidayService holidayService;
 
     @GetMapping("/create/{year}")
-    public Result create(@PathVariable String year) {
+    public Result create(@ApiParam(value = "创建年份，如2024", required = true)@PathVariable String year) {
         long rows = holidayService.insertYear(year);
         String msg = String.format("插入%d条日历数据",rows);
         Result res = Result.success(msg);
@@ -29,7 +31,8 @@ public class HolidayController {
     }
 
     @GetMapping("/interval")
-    public Result getByInterval(@RequestParam("from") String from, @RequestParam("to") String to) {
+    public Result getByInterval(@ApiParam(value = "开始日期，如2024-01-01", required = true)@RequestParam("from") String from, 
+                                @ApiParam(value = "结束日期，如2024-01-05", required = true)@RequestParam("to") String to) {
         List<Holiday> list = holidayService.query(from,to);
         String msg = String.format("查询到%d条日历数据",list==null?0:list.size());
         Result res =  Result.success(msg,list);
@@ -37,7 +40,7 @@ public class HolidayController {
     }
 
     @GetMapping("{day}")
-    public Result getByFd_day(@PathVariable("day") String day) {
+    public Result getByFd_day(@ApiParam(value = "日期，如2024-01-01", required = true)@PathVariable("day") String day) {
         Holiday holiday = holidayService.findByFd_day(day);
         Result res = Result.success("调用成功", holiday);
         return res;
